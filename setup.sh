7ce8b16b251a18d5fe1817554498cfa0c882aaf1
Al <achandec@villanova.edu>
2020-05-12 23:58:45 -0400

    setup.sh now installs dependecies via pip3 install -r requirements.txt

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Environment setup
+pip3 install -r requirements.txt
+
 # Download dlib's facial landmark detector file to ./dlib_data 
 cd dlib_data
 wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
